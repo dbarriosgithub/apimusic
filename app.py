162f1b399b263648a423978c94b246bca5b489d4
dbarriosgithub <dairo0216@gmail.com>
2022-04-04 20:30:42 -0500

    adding  evironment variables

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_restful import Api
 from database.db import initialize_db
@@ -7,11 +8,12 @@ app = Flask(__name__)
 api = Api(app)
 
 app.config['MONGO_SETTINGS'] = {
-      'host' : 'mongodb+srv://mbdbarrios:Db4rr10smongo@cluster0.9sguj.mongodb.net/dbmusic?retryWrites=true&w=majority'
+      'host' : os.environ['MONGODB_URI']
+      # 'host' : 'mongodb+srv://mbdbarrios:Db4rr10smongo@cluster0.9sguj.mongodb.net/dbmusic?retryWrites=true&w=majority'
 }
 
 initialize_db(app)
 initialize_routes(api)
 
-app.debug = True
+app.debug = False
 app.run()
